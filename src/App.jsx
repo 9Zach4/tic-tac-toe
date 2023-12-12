85112daece6ed4b19ab726ecce827809821dc33b
9Zach4 <djessitokoto@gmail.com>
2023-12-12 15:39:22 +0100

    init tic-tac

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="bg-gradient-to-r from-yellow-600 to-red-600 h-screen">
+          <div>
+            <h1 className="py-5 text-6xl text-center text-white font-bold">Tic Tac</h1>
+          </div>
+        </div>
+    )
+}
+
+export default App
