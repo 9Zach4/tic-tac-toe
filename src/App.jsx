6680ce77c60746d7eb9e0dd7a13814b5e73a650b
Zach <djessitokoto@gmail.com>
2023-12-23 16:18:31 +0100

    title

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <div className="bg-gradient-to-r from-yellow-600 to-red-600 h-screen">
           <div>
-            <h1 className="py-5 text-6xl text-center text-white font-bold">Tic Tac</h1>
+            <h1 className="py-5 text-6xl text-center text-white font-bold">Tic Tac Toe</h1>
         </div>
         <div className="flex justify-center">
           <Grid/>
